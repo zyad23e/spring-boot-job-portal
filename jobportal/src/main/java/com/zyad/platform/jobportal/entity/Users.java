827d4df254a6d9e54dea13a1c893f5b5597c9090
zyad23e <zyade23@arizona.edu>
2026-01-05 12:49:36 -0700

    Created entity package with 2 entity classes, annotated each class correctly to map to the database and also set up relationships between the 2 '@ManyToOne and @OneToMany

---
+++ jobportal/src/main/java/com/zyad/platform/jobportal/entity/Users.java
@@ -0,0 +1,107 @@
+package com.zyad.platform.jobportal.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+
+    // remember that field name = column name
+    // unique = true means no duplicate emails allowed! an exception is thrown that we have to handle if a duplicate it inputted.
+    @Column(unique = true)
+    private String email;
+
+    private boolean isActive;
+
+    @NotEmpty
+    private String password;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date registrationDate;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    // name field here is saying in the USERS table there is a field named userTypeId
+    @JoinColumn(name = "userTypeId", referencedColumnName = "userTypeId")
+    private UsersType userTypeId;
+
+    public Users(){
+
+    }
+
+    public Users(int userId, String email, String password, boolean isActive, Date registrationDate, UsersType userTypeId) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.isActive = isActive;
+        this.registrationDate = registrationDate;
+        this.userTypeId = userTypeId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public @NotEmpty String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotEmpty String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public UsersType getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(UsersType userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", isActive=" + isActive +
+                ", registrationDate=" + registrationDate +
+                ", userTypeId=" + userTypeId +
+                '}';
+    }
+}
---
+++ jobportal/src/main/java/com/zyad/platform/jobportal/entity/UsersType.java
@@ -0,0 +1,60 @@
+package com.zyad.platform.jobportal.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users_type")
+public class UsersType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userTypeId;
+
+    private String userTypeName;
+
+    @OneToMany(mappedBy = "userTypeId", cascade = CascadeType.ALL)
+    private List<Users> users;
+
+    public UsersType() {
+    }
+
+    public UsersType(int userTypeId, List<Users> users, String userTypeName) {
+        this.userTypeId = userTypeId;
+        this.users = users;
+        this.userTypeName = userTypeName;
+    }
+
+    public int getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(int userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public String getUserTypeName() {
+        return userTypeName;
+    }
+
+    public void setUserTypeName(String userTypeName) {
+        this.userTypeName = userTypeName;
+    }
+
+    public List<Users> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<Users> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "UsersType{" +
+                "userTypeId=" + userTypeId +
+                ", userTypeName='" + userTypeName + '\'' +
+                '}';
+    }
+}
