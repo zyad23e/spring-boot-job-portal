70ecf27f86f8daedeeef1de81041bd99e73c52ac
zyad23e <zyade23@arizona.edu>
2026-01-07 09:57:42 -0700

    added support for uplading a profile photo for the recruiter

--- jobportal/src/main/java/com/zyad/platform/jobportal/controller/RecruiterProfileController.java
+++ jobportal/src/main/java/com/zyad/platform/jobportal/controller/RecruiterProfileController.java
@@ -12,7 +12,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.StringUtils;
 
+
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -47,5 +52,26 @@ public class RecruiterProfileController {
             }
         }
         return "recruiter_profile";
+    }
+
+    public String addNew(RecruiterProfile recruiterProfile, @RequestParam("image")MultipartFile multipartFile, Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)){
+            String currentUsername = authentication.getName();
+            Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("Could not find user."));
+            recruiterProfile.setUserId(users);
+            recruiterProfile.setUserAccountId(users.getUserId());
+        }
+        // model.addAttribute -> adds the recruiterProfile object to the Spring MVC model. makes it available to the view under the name "profile".
+        model.addAttribute("profile", recruiterProfile);
+        String fileName = "";
+        // only run the image-handling code if the user selected a file.
+        if (!multipartFile.getOriginalFilename().equals("")){
+            fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+            recruiterProfile.setProfilePhoto(fileName);
+        }
+        RecruiterProfile savedUser = recruiterProfileService.addNew(recruiterProfile);
+        String uploadDir = "photos/recruiter/" + savedUser.getUserAccountId();
+
     }
 }
