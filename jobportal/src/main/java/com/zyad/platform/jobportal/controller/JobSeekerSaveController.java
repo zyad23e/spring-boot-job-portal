699e875952b45a90566b71540821b2689727e3c1
zyad23e <zyade23@arizona.edu>
2026-01-08 12:55:59 -0700

    added new html form for saved jobs and refactored 2 classes to save a job for a candidate

---
+++ jobportal/src/main/java/com/zyad/platform/jobportal/controller/JobSeekerSaveController.java
@@ -0,0 +1,71 @@
+package com.zyad.platform.jobportal.controller;
+
+import com.zyad.platform.jobportal.entity.*;
+import com.zyad.platform.jobportal.services.JobPostActivityService;
+import com.zyad.platform.jobportal.services.JobSeekerProfileService;
+import com.zyad.platform.jobportal.services.JobSeekerSaveService;
+import com.zyad.platform.jobportal.services.UsersService;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class JobSeekerSaveController {
+
+    private final UsersService usersService;
+    private final JobSeekerProfileService jobSeekerProfileService;
+    private final JobPostActivityService jobPostActivityService;
+    private final JobSeekerSaveService jobSeekerSaveService;
+
+    public JobSeekerSaveController(UsersService usersService, JobSeekerProfileService jobSeekerProfileService, JobPostActivityService jobPostActivityService, JobSeekerSaveService jobSeekerSaveService) {
+        this.usersService = usersService;
+        this.jobSeekerProfileService = jobSeekerProfileService;
+        this.jobPostActivityService = jobPostActivityService;
+        this.jobSeekerSaveService = jobSeekerSaveService;
+    }
+
+    @PostMapping("job-details/save/{id}")
+    public String save(@PathVariable("id") int id, JobSeekerSave jobSeekerSave){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)){
+            String currentUsername = authentication.getName();
+            Users users = usersService.findByEmail(currentUsername);
+            Optional<JobSeekerProfile> seekerProfile = jobSeekerProfileService.getOne(users.getUserId());
+            JobPostActivity jobPostActivity = jobPostActivityService.getOne(id);
+            if (seekerProfile.isPresent() && jobPostActivity != null) {
+                jobSeekerSave = new JobSeekerSave();
+                jobSeekerSave.setJob(jobPostActivity);
+                jobSeekerSave.setUserId(seekerProfile.get());
+            } else {
+                throw new RuntimeException("User not found");
+            }
+            jobSeekerSaveService.addNew(jobSeekerSave);
+        }
+        return "redirect:/dashboard/";
+    }
+
+    @GetMapping("saved-jobs/")
+    public String savedJobs(Model model){
+        List<JobPostActivity> jobPost = new ArrayList<>();
+        Object currentUserProfile = usersService.getCurrentUserProfile();
+
+        List<JobSeekerSave> jobSeekerSaveList = jobSeekerSaveService.getCandidatesJob((JobSeekerProfile) currentUserProfile);
+        for (JobSeekerSave jobSeekerSave: jobSeekerSaveList){
+            jobPost.add(jobSeekerSave.getJob());
+        }
+        model.addAttribute("jobPost", jobPost);
+        model.addAttribute("user", currentUserProfile);
+
+        return "saved-jobs";
+    }
+}
